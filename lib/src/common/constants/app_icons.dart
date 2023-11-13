7525172a289b184e408ae8dc80444b7fb42bc2b9
Murodjon <murodjonerkinov2005@gmail.com>
2023-11-13 17:50:46 +0500

    added icons

---
+++ lib/src/common/constants/app_icons.dart
@@ -0,0 +1,7 @@
+abstract class AppIcons {
+  static const _path = "assets/icons";
+  static const back = "$_path/back.svg";
+  static const calendar = "$_path/calendar.svg";
+  static const clock = "$_path/clock.svg";
+  static const settings = "$_path/set.svg";
+}
--- lib/src/common/constants/weather_icons.dart
+++ lib/src/common/constants/weather_icons.dart
@@ -1,3 +1,8 @@
 abstract class WeathersIcons {
-
+  static const _path = "assets/weathers";
+  static const cloudy = "$_path/cloudy.svg";
+  static const rain = "$_path/rain.svg";
+  static const sunCloudy = "$_path/sun_cloudy.svg";
+  static const sunny = "$_path/sunny.svg";
+  static const thunder = "$_path/thunder.svg";
 }

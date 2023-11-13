96477907a2c550c48712ae782a79fb84fe5c6b3c
Murodjon <murodjonerkinov2005@gmail.com>
2023-11-13 16:23:24 +0500

    first commit

--- android/app/src/main/kotlin/com/example/weather_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/weather_app/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.weather_app
 
+import androidx.annotation.NonNull
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 import com.yandex.mapkit.MapKitFactory
 
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity() {
+class MainActivity: FlutterActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        MapKitFactory.setLocale("YOUR_LOCALE") // Your preferred language. Not required, defaults to system language
+//        MapKitFactory.setLocale("YOUR_LOCALE") // Your preferred language. Not required, defaults to system language
         MapKitFactory.setApiKey("911ead18-c5d2-41ab-a2c8-6e10b0d34309") // Your generated API key
         super.configureFlutterEngine(flutterEngine)
     }
